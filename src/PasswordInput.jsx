f1369531926030d3f5ad774293cf3ffa522d60a8
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-09-25 09:01:20 +0000

    Refactored

--- src/PasswordInput.jsx
+++ src/PasswordInput.jsx
@@ -6,9 +6,6 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
-// // import FilledInput from '@mui/material/FilledInput';
-// import FormHelperText from '@mui/material/FormHelperText';
-// import TextField from '@mui/material/TextField';
 
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
