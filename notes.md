b1f46a2dd4a769693b26aa6b1cc794122c4e5817
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-09-25 09:00:53 +0000

    Initial commit

---
+++ notes.md
@@ -0,0 +1,4 @@
+# Links
+
+- https://mui.com/components/text-fields/#InputAdornments.js
+- https://codesandbox.io/s/gnnwq?file=/demo.js
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import InputAdornments from './InputAdornments';
+import PasswordInput from './PasswordInput';
+
+function App() {
+  return (
+    <main>
+      <h3>Password input with show / hide icon</h3>
+      <PasswordInput />
+    </main>
+  );
+}
+
+export default App;
